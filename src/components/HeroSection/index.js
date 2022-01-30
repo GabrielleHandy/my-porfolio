6e0554d1c5a14a8dd215724075088a9728c64c96
Gabrielle <gehandy123@gmail.com>
2022-01-30 05:17:59 +0000

    Fixed some design bugs(video, background image)

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1} from './HeroElements'
-
+import Video from '../../videos/video.mp4'
 const HeroSection = () => {
     return (
         <HeroContainer id="main">
             <HeroBg>
-                <VideoBg autoPlay loop muted src={"../../videos/video.mp4"} type='video/mp4'>
+                <VideoBg autoPlay loop muted src={require(Video)} type='video/mp4'>
 
                 </VideoBg>
             </HeroBg>
